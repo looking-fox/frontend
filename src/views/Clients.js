45c1181661c6d2c0a51bcb333d05976de71d0207
Ryan B <brockhoffrw@gmail.com>
2020-01-10 10:21:07 -0500

    refactor: remove example modal from Client View.

--- src/views/Clients.js
+++ src/views/Clients.js
@@ -1,17 +1,9 @@
 import React, { useState } from "react";
-import { Modal, Text } from "../ui/StyledComponents";
 
 const Clients = () => {
-  const [showModal, setShowModal] = useState(true);
-  const toggle = () => {
-    setShowModal(!showModal);
-  };
   return (
     <div style={{ background: "#777777", height: "calc(100vh - 60px)" }}>
       <p onClick={toggle}>Clients</p>
-      <Modal showModal={showModal} onClose={toggle} simpleModal>
-        <Text>Wow what a good title.</Text>
-      </Modal>
     </div>
   );
 };
