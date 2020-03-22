00db7f170667af1aead03e76877937338d7c5bfa
Ryan B <brockhoffrw@gmail.com>
2020-03-21 17:14:16 -0700

    feat: formik for complex forms.

--- src/components/Clients/AddClient/ClientModal.js
+++ src/components/Clients/AddClient/ClientModal.js
@@ -4,14 +4,18 @@ import { getWorkflows } from "../../../thunks/workflowThunks";
 import styled from "styled-components";
 import { Button, Modal, Input, TextArea } from "../../../ui/StyledComponents";
 import Select from "react-select";
+import { Formik, Form, Field, ErrorMessage } from "formik";
 
 class ClientModal extends Component {
-  state = { workflowOptions: [{ label: "Choose Workflow...", value: null }] };
-
   async componentDidMount() {
     await this.props.getWorkflows();
     await this.generateOptions();
   }
+
+  handleInput = ({ target }) => {
+    const { value, name } = target;
+    this.setState({ [name]: value }, () => console.log(this.state));
+  };
 
   generateOptions = () => {
     const workflowOptions = this.props.workflows.map((wf, idx) => {
@@ -20,22 +24,50 @@ class ClientModal extends Component {
     this.setState({ workflowOptions });
   };
 
+  handleValidation = values => {
+    const errors = {};
+    if (!values.email) {
+      errors.email = "Required";
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
+      errors.email = "Invalid email address";
+    }
+    return errors;
+  };
+
+  handleSubmitForm = (values, { setSubmitting }) => {
+    setTimeout(() => {
+      alert(JSON.stringify(values, null, 2));
+      setSubmitting(false);
+    }, 400);
+  };
+
   render() {
     const { showModal, handleToggleClientModal } = this.props;
-    const { workflowOptions } = this.state;
+    const initialFormState = { name: "", email: "", phone: "" };
     return (
       <Modal showModal={true} onClose={handleToggleClientModal}>
-        <Input placeholder="Name" />
-        <Input placeholder="Email" />
-        <Input placeholder="Phone #" />
-        <StyledSelect
-          options={workflowOptions}
-          defaultValue={workflowOptions[0]}
-          onChange={({ value }) => alert("Value: ", value)}
-        />
-        <Input type="date" />
-        <TextArea />
-        <AddButton>Add Client</AddButton>
+        <Formik
+          initialValues={initialFormState}
+          validate={this.handleValidation}
+          onSubmit={this.handleSubmitForm}
+        >
+          {({ isSubmitting }) => (
+            <Form>
+              <Field type="name" name="name" />
+              <StyErrorMessage name="name" component="div" />
+
+              <Field type="email" name="email" />
+              <StyErrorMessage name="email" component="div" />
+
+              <Field type="phone" name="phone" />
+              <StyErrorMessage name="phone" component="div" />
+
+              <AddButton type="submit" disabled={isSubmitting}>
+                Add Client
+              </AddButton>
+            </Form>
+          )}
+        </Formik>
       </Modal>
     );
   }
@@ -50,6 +82,11 @@ const StyledSelect = styled(Select)`
   margin: 10px 0px;
 `;
 
+const StyErrorMessage = styled(ErrorMessage)`
+  color: red;
+  margin: 10px 0px;
+`;
+
 const mapState = state => {
   return { workflows: state.workflow.workflows };
 };
@@ -57,3 +94,57 @@ const mapState = state => {
 const mapDispatch = { getWorkflows };
 
 export default connect(mapState, mapDispatch)(ClientModal);
+
+{
+  /* <Input
+          placeholder="Name"
+          name="clientName"
+          onChange={this.handleInput}
+        />
+        <Input
+          placeholder="Email"
+          name="clientEmail"
+          onChange={this.handleInput}
+        />
+        <Input
+          placeholder="Phone #"
+          name="clientPhoneNumber"
+          onChange={this.handleInput}
+        />
+        <StyledSelect
+          options={workflowOptions}
+          defaultValue={workflowOptions[0]}
+          onChange={({ value }) => alert("Value: ", value)}
+        />
+        <Input type="date" name="clientDate" onChange={this.handleInput} />
+        <TextArea
+          placeholder="Custom Note..."
+          name="clientNote"
+          onChange={this.handleInput}
+        /><Input
+        placeholder="Name"
+        name="clientName"
+        onChange={this.handleInput}
+      />
+      <Input
+        placeholder="Email"
+        name="clientEmail"
+        onChange={this.handleInput}
+      />
+      <Input
+        placeholder="Phone #"
+        name="clientPhoneNumber"
+        onChange={this.handleInput}
+      />
+      <StyledSelect
+        options={workflowOptions}
+        defaultValue={workflowOptions[0]}
+        onChange={({ value }) => alert("Value: ", value)}
+      />
+      <Input type="date" name="clientDate" onChange={this.handleInput} />
+      <TextArea
+        placeholder="Custom Note..."
+        name="clientNote"
+        onChange={this.handleInput}
+      /> */
+}
